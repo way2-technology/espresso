521a905350693b93165b842498c1db69bf27d13a
Andre Carlucci <andrecarlucci@gmail.com>
2015-05-14 17:26:40 -0300

    degugugugugu 2

--- Espresso/Controllers/ValuesController.cs
+++ Espresso/Controllers/ValuesController.cs
@@ -1,17 +1,33 @@
 ﻿using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Web.Http;
+using MiniBiggy;
 
 namespace Espresso.Controllers {
     public class ValuesController : ApiController {
         
         public IEnumerable<string> Get() {
-            return new string[] { "value1", "value2" };
+            return PersistentList.Create<Log>().Select(x => x.Text);
         }
 
         [Route("dados")]
         public void Post([FromBody]string value) {
-            Debug.WriteLine("POST -> " + value);
+            var log = new Log(value);
+            var list = PersistentList.Create<Log>();
+            list.AutoSave = true;
+            list.Add(log);
+        }
+    }
+
+    public class Log {
+        public string Text { get; set; }
+
+        public Log(string text) {
+            Text = text;
+        }
+
+        public Log() {
         }
     }
 }
