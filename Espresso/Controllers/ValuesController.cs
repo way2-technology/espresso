d2cb405e07cabc7718da25e247565687c348b31e
Andre Carlucci <andrecarlucci@gmail.com>
2015-05-14 17:45:02 -0300

    file

--- Espresso/Controllers/ValuesController.cs
+++ Espresso/Controllers/ValuesController.cs
@@ -3,18 +3,19 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Web;
 using System.Web.Http;
 
 namespace Espresso.Controllers {
     public class ValuesController : ApiController {
         
         public IEnumerable<string> Get() {
-            return File.ReadAllLines("coffee.txt");
+            return File.ReadAllLines(HttpContext.Current.Server.MapPath("~/") + "coffee.txt");
         }
 
         [Route("dados")]
         public void Post([FromBody]string value) {
-            File.AppendAllText("coffee.txt", value + Environment.NewLine);
+            File.AppendAllText(HttpContext.Current.Server.MapPath("~/") + "coffee.txt", value + Environment.NewLine);
         }
     }
 
